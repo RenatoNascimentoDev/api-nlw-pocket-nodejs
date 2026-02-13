75c5ac85c2ba461be0099f6b4ccc1d0239baed08
Renato Nascimento <renatocorreianascimento@gmail.com>
2026-02-12 23:27:03 -0300

    criação de tabelas e seed

---
+++ .migrations/0001_lying_morgan_stark.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "goals_completations" (
+	"id" text PRIMARY KEY NOT NULL,
+	"goal_id" text NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "goals_completations" ADD CONSTRAINT "goals_completations_goal_id_goals_id_fk" FOREIGN KEY ("goal_id") REFERENCES "public"."goals"("id") ON DELETE no action ON UPDATE no action;
